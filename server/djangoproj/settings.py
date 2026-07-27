0e0f2891cc1c9571bab6d54ded3fd664ed3a22ad
ThomasCaruso <thomas@webhorizondigital.com>
2026-07-27 10:57:11 -0600

    Serve production static assets with WhiteNoise

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -101,6 +102,14 @@ STATICFILES_DIRS = [
     for directory in [FRONTEND_STATIC_DIR, FRONTEND_BUILD_DIR / "static"]
     if directory.exists()
 ]
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
